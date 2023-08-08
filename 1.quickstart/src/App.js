d22b80776801028dbdd684807d4f0346320d36b3
Planting12 <simej1214@gmail.com>
2023-08-08 22:45:39 +0900

    ✨ feat: 컴포넌트 간 데이터 공유 실습

--- 1.quickstart/src/App.js
+++ 1.quickstart/src/App.js
@@ -1,9 +1,18 @@
-import Profile from "./myButton";
+import MyButton from "./myButton";
+import { useState } from "react";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
   return (
     <>
-      <Profile />
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
     </>
   );
 }
--- 1.quickstart/src/myButton.jsx
+++ 1.quickstart/src/myButton.jsx
@@ -1,13 +1,3 @@
-const products = [
-  { title: "Cabbage", id: 1 },
-  { title: "Garlic", id: 2 },
-  { title: "Apple", id: 3 },
-];
-
-export default function Profile(user) {
-  const listItems = products.map((products) => (
-    <li key={products.id}>{products.title}</li>
-  ));
-
-  return <ul>{listItems}</ul>;
+export default function MyButton({ count, onClick }) {
+  return <button onClick={onClick}>Clicked {count} times</button>;
 }
